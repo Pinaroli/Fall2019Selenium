4e6a683fbff6682a5351454c7d490886a01fb696
Fatmaual <fatmaulusal@gmail.com>
2020-03-01 15:18:32 -0500

    Sunday' Code1

--- src/test/java/com/automation/test/day2/BasicNavigation.java
+++ src/test/java/com/automation/test/day2/BasicNavigation.java
@@ -12,8 +12,18 @@ public class BasicNavigation {
         // ChromeDriver driver= new ChromeDriver();
         // RemoteWebDriver driver1=new SafariDriver();
         WebDriver driver=new ChromeDriver();
-        driver.get("http:google.com");//for openning the website this is the key of the open the door of website
+        //for openning the website this is the key of the open the door of website
+        driver.get("http:google.com");
+        driver.manage().window().maximize();
+      //  driver.manage().window().fullscreen();
         Thread.sleep(6000);
+        //go to another website with the same window
+        driver.navigate().to("http://amazon.com");
+        if(driver.getTitle().toLowerCase().contains("amazon")){
+            System.out.println("test passed");
+        }else{
+            System.out.println("test failed");
+        }
         //method that return page title
         //you can also see as tab name,in the browser
         String title=driver.getTitle();//returns <title>some title</title> text
@@ -29,5 +39,12 @@ public class BasicNavigation {
         //must be at the end
         driver.close();// to close browser without this method the browser can't close
         //this driver close method has to be end other than that we can't get the connect
-    }
+        }
+        public static void verifyEquals(String arg1,String arg2){
+            if (arg1.equals(arg2)) {
+                System.out.println("test passed");
+            }else{
+                System.out.println("test failed");
+            }
+        }
 }
